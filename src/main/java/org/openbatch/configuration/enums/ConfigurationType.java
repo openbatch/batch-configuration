81900bc9e2a91e0f690fc6ee3903fe6643d30bb7
Larry Shields <larry.shields@gmail.com>
2022-01-30 16:48:37 -0500

    Added ConfigurationType Enum

---
+++ src/main/java/org/openbatch/configuration/enums/ConfigurationType.java
@@ -0,0 +1,14 @@
+package org.openbatch.configuration.enums;
+
+/**
+ * Enumeration of Canned Configuration types for General Configurations and External Settings.
+ */
+public enum ConfigurationType {
+  BASE,
+  GENERAL,
+  DATABASE,
+  CLOUD,
+  FILE,
+  API,
+  OTHER
+}
